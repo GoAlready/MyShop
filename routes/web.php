e80eb75778ac67fdcd16a402da5ef5bb7a0cdd6c
Xavier <NatsumeXavier@gmail.com>
2018-10-31 09:50:17 +0800

    路由分组

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,28 @@
     
     Route::group(['prefix'=>'home','namespace'=>'Home'],function()
     {
-        // 前台登录注册
-        Route::get('/login','LoginController@login')->name('home_login');
-        Route::post('/login','LoginController@dologin')->name('home_dologin');
+        // 登录注册
+        {
+            // 前台登录注册页面
+            Route::get('/login','LoginController@login')->name('home_login');
+            Route::get('/register','RegisterController@register')->name('home_register');
 
-        Route::get('/register','RegisterController@register')->name('home_register');
-        Route::post('/register','RegisterController@doregister')->name('home_doregister');
+            // 前台登录注册数据
+            Route::post('/login','LoginController@dologin')->name('home_dologin');
+            Route::post('/register','RegisterController@doregister')->name('home_doregister');
+        }
 
         // 发送短信验证
         Route::get('/sendmobile','RegisterController@send_mobile')->name('sendmobile');
 
-
         Route::middleware(['home_login'])->group(function(){
-
-            // 退出
+            // 退出登录
             Route::get('/logout','LoginController@logout')->name('home_logout');
-        
 
         }); 
     });
 
+    // 后台管理
     Route::group(['prefix'=>'admin','namespace'=>'Admin'],function()
     {
         // 后台登录
@@ -33,44 +35,57 @@
 
         Route::middleware(['login'])->group(function(){
             // 后台首页
-            Route::get('/index','IndexController@index')->name('admin_index');
+            Route::get('/','IndexController@index')->name('admin_index');
             Route::get('/home','IndexController@home')->name('admin_home');
             // 退出
             Route::get('/logout','LoginController@logout')->name('admin_logout');
+            
             // 文章管理
-            Route::get('/artlist','ArticleController@list')->name('admin_artlist');
-            Route::get('/artcreate','ArticleController@create')->name('admin_artcreate');
-            Route::post('/artadd','ArticleController@add')->name('admin_artadd');
-            Route::get('/artedit/{id}','ArticleController@edit')->name('admin_artedit');
-            Route::get('/artupdate','ArticleController@update')->name('admin_artupdate');
-            Route::get('/artdelete','ArticleController@delete')->name('admin_artdelete');
+            {
+                Route::get('/artlist','ArticleController@list')->name('admin_artlist');
+                Route::get('/artcreate','ArticleController@create')->name('admin_artcreate');
+                Route::post('/artadd','ArticleController@add')->name('admin_artadd');
+                Route::get('/artedit/{id}','ArticleController@edit')->name('admin_artedit');
+                Route::get('/artupdate','ArticleController@update')->name('admin_artupdate');
+                Route::get('/artdelete','ArticleController@delete')->name('admin_artdelete');
+            }
 
             // 文章分类管理
-            Route::get('/artsort','Article_cateController@sort')->name('admin_artsort');
-            Route::post('/artsort_add','Article_cateController@add')->name('admin_artsort_add');
-            Route::get('/artsort_edit/{id}','Article_cateController@edit')->name('admin_artsort_edit');
-            Route::post('/artsort_update/{id}','Article_cateController@update')->name('admin_artsort_update');
-            Route::get('/artsort_delete','Article_cateController@delete')->name('admin_artsort_delete');
+            {
+                Route::get('/artsort','Article_cateController@sort')->name('admin_artsort');
+                Route::post('/artsort_add','Article_cateController@add')->name('admin_artsort_add');
+                Route::get('/artsort_edit/{id}','Article_cateController@edit')->name('admin_artsort_edit');
+                Route::post('/artsort_update/{id}','Article_cateController@update')->name('admin_artsort_update');
+                Route::get('/artsort_delete','Article_cateController@delete')->name('admin_artsort_delete');
+            }
 
             // 管理员管理
-            Route::get('/admin_list','AdminController@list')->name('admin_adminlist');
-            Route::post('/admin_add','AdminController@add')->name('admin_adminadd');
-            Route::get('/admin_edit/{id}','AdminController@edit')->name('admin_adminedit');
-            Route::post('/admin_update/{id}','AdminController@update')->name('admin_adminupdate');
-            Route::get('/admin_delete','AdminController@delete')->name('admin_admindelete');
+            {   
+                Route::get('/admin_list','AdminController@list')->name('admin_adminlist');
+                Route::post('/admin_add','AdminController@add')->name('admin_adminadd');
+                Route::get('/admin_edit/{id}','AdminController@edit')->name('admin_adminedit');
+                Route::post('/admin_update/{id}','AdminController@update')->name('admin_adminupdate');
+                Route::get('/admin_delete','AdminController@delete')->name('admin_admindelete');
+            }
+
             // 角色管理
-            Route::get('/role_list','RoleController@list')->name('admin_rolelist');
-            Route::get('/role_create','RoleController@create')->name('admin_rolecreate'); 
-            Route::post('/role_add','RoleController@add')->name('admin_roleadd'); 
-            Route::get('/role_edit','RoleController@edit')->name('admin_roleedit'); 
-            Route::post('/role_update','RoleController@create')->name('admin_roleupdate'); 
+            {
+                Route::get('/role_list','RoleController@list')->name('admin_rolelist');
+                Route::get('/role_create','RoleController@create')->name('admin_rolecreate'); 
+                Route::post('/role_add','RoleController@add')->name('admin_roleadd'); 
+                Route::get('/role_edit','RoleController@edit')->name('admin_roleedit'); 
+                Route::post('/role_update','RoleController@create')->name('admin_roleupdate'); 
+            }
 
             // 权限管理
-            Route::get('/pri_list','PrivilegeController@list')->name('admin_prilist');
-            Route::get('/pri_create','PrivilegeController@create')->name('admin_pricreate'); 
-            Route::post('/pri_add','PrivilegeController@add')->name('admin_priadd'); 
-            Route::get('/pri_edit','PrivilegeController@edit')->name('admin_priedit'); 
-            Route::post('/pri_update','PrivilegeController@create')->name('admin_priupdate'); 
+            {
+                Route::get('/pri_list','PrivilegeController@list')->name('admin_prilist');
+                Route::get('/pri_create','PrivilegeController@create')->name('admin_pricreate'); 
+                Route::post('/pri_add','PrivilegeController@add')->name('admin_priadd'); 
+                Route::get('/pri_edit','PrivilegeController@edit')->name('admin_priedit'); 
+                Route::post('/pri_update','PrivilegeController@create')->name('admin_priupdate'); 
+            }
+
             // 会员管理
 
             // 商品管理
